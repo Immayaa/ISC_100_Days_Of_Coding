57379765a157b071308734f5b1f5f8fa2e4bc228
Immayaa <lismawativchu@gmail.com>
2023-10-09 16:35:08 +0700

    hari1

---
+++ day1_biodata.java
@@ -0,0 +1,22 @@
+
+package immayaa;
+
+
+public class day1_biodata {
+    public static void main(String[] args) {
+        String nama = "lismawati";
+        String nim = "D0222037";
+        String kelas = "informatika E";
+        String alamat ="Tapango";
+        int angkatan = 2022;
+
+        System.out.println("NAMA     :"+ nama);
+        System.out.println("NIM      :"+ nim);
+        System.out.println("KELAS    :"+ kelas);
+        System.out.println("ALAMAT   :"+ alamat);
+        System.out.println("ANGKATAN :"+ angkatan);
+
+    }
+
+}
+
