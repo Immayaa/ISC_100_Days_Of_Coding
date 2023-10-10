826e164b22dd034ad54323242c130eb4d1aa092a
Immayaa <lismawativchu@gmail.com>
2023-10-10 14:19:33 +0700

    hari2

---
+++ day2_TipeDataPrimitif.java
@@ -0,0 +1,28 @@
+
+package immayaa;
+
+
+public class day2_TipeDataPrimitif {
+    public static void main(String[] args) {
+        int iniInteger =2002;
+        byte iniByte   =21;
+        char iniChar   = 'A';
+        short iniShort =45;
+        float iniFloat =1.53f;
+        double iniDouble =1.24;
+        boolean iniBolean =true;
+        long iniLong =1122334455;  
+        
+        
+        System.out.println("INT     :" + iniInteger);
+        System.out.println("BYTE    :" + iniByte);
+        System.out.println("CHAR    :" + iniChar);
+        System.out.println("SHORT   :" + iniShort);
+        System.out.println("FLOT    :" + iniFloat);
+        System.out.println("DOUBLE  :" + iniDouble);
+        System.out.println("BOOLEAN :" + iniBolean);
+        System.out.println("LONG    :" + iniLong);
+    }
+    
+    
+}
