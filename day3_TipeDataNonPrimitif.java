801f298a3d9d2d1ba7b97015432f05c9ff647bf7
Immayaa <147224660+Immayaa@users.noreply.github.com>
2023-10-11 19:44:16 +0700

    Add files via upload

---
+++ day3_TipeDataNonPrimitif.java
@@ -0,0 +1,13 @@
+
+package immayaa;
+
+
+public class day3_TipeDataNonPrimitif {
+    public static void main(String[] args) {
+        string iniString = "immayaa";
+        byte iniByte = 21;
+        short iniShort =2002;
+        
+    }
+    
+}
