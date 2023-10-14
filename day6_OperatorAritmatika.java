6ea4ca3a6814c5ebf11dbf14b20a5e7d8935b922
Immayaa <147224660+Immayaa@users.noreply.github.com>
2023-10-14 18:18:13 +0700

    hari6

---
+++ day6_OperatorAritmatika.java
@@ -0,0 +1,33 @@
+package immayaa;
+
+public class day6_OperatorAritmatika {
+
+    public static void main(String[] args) {
+        int b1 = 15;
+        int b2 = 5;
+
+        //penjumlahan
+        int hasil = b1 + b2;
+        System.out.println(b1 + b2);
+
+        //pengurangan 
+        hasil = b1 - b2;
+        System.out.println(b1 - b2);
+
+        //perkalian
+        hasil = b1 * b2;
+        System.out.println(b1 * b2);
+
+        //pembagian
+        float a = 8;
+        float b = 5;
+        float hasilfloat = a / b;
+        System.out.println(a / b);
+
+        //modulus
+        hasilfloat = a % b;
+        System.out.println(a % b);
+
+    }
+
+}
