b63e2ad0287a86e6d14d8832bf83bb0b8197a776
Immayaa <147224660+Immayaa@users.noreply.github.com>
2023-10-22 22:05:47 +0800

    hari14

---
+++ day14_kondisi percabangan.java
@@ -0,0 +1,13 @@
+public class day14_KondisiPercabangan{
+	public static void main(String[] args) {
+		int harga = 30;
+		
+		System.out.println("harga barang = " + harga);
+		
+		if (harga > 30){
+			System.out.println("harga barang mahal ");
+	}else {
+		System.out.println("harga barang murah ");
+	}
+	}
+}
