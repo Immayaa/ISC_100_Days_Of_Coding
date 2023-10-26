5b9fc3efc7560a83ec9d6646d74dfa6276904a19
Immayaa <147224660+Immayaa@users.noreply.github.com>
2023-10-26 20:22:57 +0800

    hari18

---
+++ day18_nested_if.java
@@ -0,0 +1,15 @@
+package immayaa;
+public class day18 {
+    public static void main(String[] args) {
+	int umur = 25;
+		
+        if(umur < 30){
+            if(umur > 20){
+                System.out.println("anda cukup umur");
+            }
+        }
+        else{
+            System.out.println("umur anda tidak sesuai");
+        }
+    }
+}
