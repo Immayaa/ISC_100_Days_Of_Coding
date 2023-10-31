24fbdd56be93ee2d18bf975f183f58659f542caf
Immayaa <147224660+Immayaa@users.noreply.github.com>
2023-10-31 18:08:08 +0800

    hari23

---
+++ day23_whileLoop.java
@@ -0,0 +1,16 @@
+
+package immayaa;
+
+
+public class day23_whileLoop {
+    public static void main(String[] args) {
+   
+        int x = 0;
+        while (x <= 10){
+            System.out.println("immayaa : " + x);
+            x++;
+        }
+    }
+}
+
+    
