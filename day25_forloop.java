46b187ab60e7e4ecd1b3acd1f056d76c81a98343
Immayaa <147224660+Immayaa@users.noreply.github.com>
2023-11-02 20:17:27 +0800

    hari25

---
+++ day25_forloop.java
@@ -0,0 +1,14 @@
+package immayaa;
+
+public class day25_forloop {
+
+    public static void main(String[] args) {
+
+        System.out.println("ini adalah for loop");
+
+        for (int x = 0; x <= 10; x++) {
+            System.out.println("immayaa : " + x);
+
+        }
+    }
+}
