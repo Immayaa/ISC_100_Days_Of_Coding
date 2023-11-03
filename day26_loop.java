731112b87fba763cb8bd36c96fb0dcd648126963
Immayaa <147224660+Immayaa@users.noreply.github.com>
2023-11-03 23:53:04 +0800

    hari26

---
+++ day26_loop.java
@@ -0,0 +1,20 @@
+package immayaa;
+
+public class day26_Loop {
+
+    public static void main(String[] args) {
+
+        System.out.println("ini adalah for loop");
+        System.out.println("1-10");
+        for (int x = 0; x <= 10; x++) {
+            System.out.println("immayaa : " + x);
+        }
+
+        System.out.println("10-1");
+
+        for (int x = 10; x >= 0; x--) {
+            System.out.println("immayaa : " + x);
+
+        }
+    }
+}
