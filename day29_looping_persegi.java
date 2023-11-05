3972a51c65ae0fb1abc06e04ab4ae49d0806f625
Immayaa <147224660+Immayaa@users.noreply.github.com>
2023-11-05 20:19:46 +0800

    hari29

---
+++ day29_looping_persegi.java
@@ -0,0 +1,15 @@
+package immayaa;
+public class day29_looping_persegi {
+    public static void main(String[] args) {
+
+        int i,j;
+        for ( i = 0; i <=5; i++) {
+            System.out.println();
+            
+            for  (j = 0; j < 5; j++) {
+                System.out.print(" * ");
+                
+            }
+        }
+    }
+}
