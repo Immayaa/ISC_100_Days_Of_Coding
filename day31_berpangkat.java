7404ee52fe42d88264568f13355ae6acff68f2f7
Immayaa <147224660+Immayaa@users.noreply.github.com>
2023-11-08 20:58:07 +0800

    hari31

---
+++ day31_berpangkat.java
@@ -0,0 +1,14 @@
+package immayaa;
+
+import java.util.Scanner;
+
+public class day31_berpangkat {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.print("masukkan angka : ");
+        int i = a.nextInt();
+        int hasil = i * i;
+        System.out.println(hasil);
+    }
+    
+}
