4a8c23511b90a53df9b48db10889ae6f68af1576
Immayaa <147224660+Immayaa@users.noreply.github.com>
2023-11-10 23:35:48 +0800

    hari33

---
+++ day33_array_DuaDimensi.java
@@ -0,0 +1,22 @@
+
+package immayaa;
+
+
+public class day33_arrayduadimensi {
+    
+    public static void main(String[] args) {
+        int[][] angka = {{1, 2},{3, 4},{5, 6}};
+        
+
+        for (int i = 0; i < angka.length; i++) {
+            for(int j = 0; j < angka[i].length; j++) {
+                System.out.print(angka[i][j]+ " ");
+        }
+
+        System.out.println();
+        }
+    }
+}    
+
+    
+
