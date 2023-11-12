92ee600b9e0848b7bab0d76105c29c0ada937bc3
Immayaa <147224660+Immayaa@users.noreply.github.com>
2023-11-12 22:22:22 +0800

    hari35

---
+++ day35_lopping_array.java
@@ -0,0 +1,12 @@
+package immayaa;
+
+public class day35_LoopingArray {
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
