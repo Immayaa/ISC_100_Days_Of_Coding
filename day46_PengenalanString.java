f296b38f636c35fb6566b05a6f0410e5d9cc9e64
Immayaa <147224660+Immayaa@users.noreply.github.com>
2023-11-23 21:33:28 +0700

    hari46

---
+++ day46_PengenalanString.java
@@ -0,0 +1,15 @@
+package Immayaa;
+
+public class day46_PengenalanString {
+
+    public static void main(String[] args) {
+       
+        String nama = "immayaa";
+        System.out.println(nama);
+        System.out.println("karakter dari string :" + nama.charAt(1));
+        String tambahan = " itu saya";
+        String hasilGabung = nama + tambahan;
+        System.out.println("kata yang di tambahkan : " + hasilGabung);
+
+    }
+}
